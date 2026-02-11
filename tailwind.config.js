bff4fef57dfc3b6317da9b86c235c0598d0710b7
Asraful Alam <asrafulspersonal@gmail.com>
2026-02-11 11:17:52 +0600

    make the banner section

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'brand-gradient': 'linear-gradient(90deg, #FF0000 0%, #FF8938 100%)',
+      }
+    }
+  }
+}
